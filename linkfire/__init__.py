0590497bbd7abbddf25f14fc8b7f4c4c00f73c9c
Lorenzo Setale <koalalorenzo@gmail.com>
2014-03-05 14:33:49 +0100

    Fixed URL of my personal website.

--- linkfire/__init__.py
+++ linkfire/__init__.py
@@ -2,7 +2,7 @@
 """Python linkfire.com API"""
 
 __version__ = "0.1"
-__author__ = "Lorenzo Setale ( http://who.is.koalalorenzo.com/? )"
+__author__ = "Lorenzo Setale ( http://who.is.lorenzo.setale.me/? )"
 __author_email__ = "koalalorenzo@gmail.com"
 __copyright__ = "Copyright (c) 2013, 2014 Lorenzo Setale"
 
